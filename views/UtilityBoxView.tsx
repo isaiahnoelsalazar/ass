f7403e78b003b974e0abd4a82c27cb514ba24873
isaiahnoelsalazar <isaiahnoelsalazar474@gmail.com>
2026-01-02 23:06:02 +0800

    update: removed unused imports

--- views/UtilityBoxView.tsx
+++ views/UtilityBoxView.tsx
@@ -1,7 +1,6 @@
 
 // This file is deprecated. Tools have been moved to ConverterStudioView and PasswordGenView.
 import React from 'react';
-import { Navigate } from 'react-router-dom'; // Conceptually, this view is no longer used.
 
 const UtilityBoxView: React.FC = () => {
   return null;
