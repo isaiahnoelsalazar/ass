c08f75ce5be0da9406dd9b57b01b0d434c6bf552
isaiahnoelsalazar <isaiahnoelsalazar474@gmail.com>
2026-01-04 01:23:04 +0800

    test: account system remodel #33

--- services/authService.ts
+++ services/authService.ts
@@ -35,7 +35,7 @@ export const fetchTest = () => {
   requestLogin.open("GET", `https://flask-web-app-peach.vercel.app/mssql_query?&server=sql.bsite.net\MSSQL2016&database=saiasamazingaspsite_SampleDB&username=saiasamazingaspsite_SampleDB&password=DBSamplePW&query=SELECT%20%2A%20FROM%20INFORMATION_SCHEMA.TABLES%20WHERE%20TABLE_TYPE%3D%27BASE%20TABLE%27`, true);
   requestLogin.withCredentials = true;
   requestLogin.onreadystatechange = function (){
-    let response = requestLogin.responseText;
+    let response = requestLogin.status + " " + requestLogin.readyState + " " + requestLogin.responseText;
     alert(response);
   }
   requestLogin.send();
