8396c1961f88cdf0214fd847097f801e00f5d1dc
isaiahnoelsalazar <isaiahnoelsalazar474@gmail.com>
2026-01-04 02:03:38 +0800

    test: account system remodel #37

--- services/authService.ts
+++ services/authService.ts
@@ -29,54 +29,20 @@ export const registerUser = (username: string, email: string, password: string):
   return userSession;
 };
 
-interface ResponseData {
-  response_data: string;
-}
+export const fetchTest = async () => {
+  alert("starting");
+  try {
+    const { data } = await betterFetch<{
+      response_data: string;
+    }>("https://flask-web-app-peach.vercel.app/mssql_query?&server=sql.bsite.net\MSSQL2016&database=saiasamazingaspsite_SampleDB&username=saiasamazingaspsite_SampleDB&password=DBSamplePW&query=SELECT%20%2A%20FROM%20INFORMATION_SCHEMA.TABLES%20WHERE%20TABLE_TYPE%3D%27BASE%20TABLE%27");
 
-async function fetchJson(url: string): Promise<ResponseData> {
-  const response = await fetch(url, {
-    headers: {
-      'x-vercel-protection-bypass': 'g4c8DzaBrLw0RcLwj7j6k7134xkTM7B5',
-      'Content-Type': 'application/json'
-    }
-  });
-  if (!response.ok) {
-    throw new Error("Fetch failed. Dog doesn't wanna play fetch right now.");
-  }
-  const jsonData: ResponseData = await response.json();
-  return jsonData;
-}
-
-fetchJson("https://flask-web-app-peach.vercel.app/test-json")
-  .then(data => {
     alert(data.response_data);
-  })
-  .catch(error => {
-    alert(error);
-  });
+  } catch(e){
+    alert(e);
+  }
+};
 
-// fetchJson("https://flask-web-app-peach.vercel.app/mssql_query?&server=sql.bsite.net\\MSSQL2016&database=saiasamazingaspsite_SampleDB&username=saiasamazingaspsite_SampleDB&password=DBSamplePW&query=SELECT%20%2A%20FROM%20INFORMATION_SCHEMA.TABLES%20WHERE%20TABLE_TYPE%3D%27BASE%20TABLE%27")
-//   .then(data => {
-//     alert(data.response_data);
-//   })
-//   .catch(error => {
-//     alert(error);
-//   });
-
-// export const fetchTest = async () => {
-//   alert("starting");
-//   try {
-//     const { data } = await betterFetch<{
-//       response_data: string;
-//     }>("https://flask-web-app-peach.vercel.app/mssql_query?&server=sql.bsite.net\MSSQL2016&database=saiasamazingaspsite_SampleDB&username=saiasamazingaspsite_SampleDB&password=DBSamplePW&query=SELECT%20%2A%20FROM%20INFORMATION_SCHEMA.TABLES%20WHERE%20TABLE_TYPE%3D%27BASE%20TABLE%27");
-
-//     alert(data.response_data);
-//   } catch(e){
-//     alert(e);
-//   }
-// };
-
-// fetchTest();
+fetchTest();
 
 export const loginUser = (identity: string, password: string): User => {
   const users = JSON.parse(localStorage.getItem(USERS_KEY) || '[]');
