fef332c99d1a69d9b201dda54193ad21fe5a0b58
isaiahnoelsalazar <isaiahnoelsalazar474@gmail.com>
2026-01-06 22:33:52 +0800

    test: sqlite3 #4

--- views/ERDStudioView.tsx
+++ views/ERDStudioView.tsx
@@ -116,13 +116,15 @@ try:
     tables = cursor.fetchall()
     schema = "\\n".join([t[0] for t in tables if t[0]])
     conn.close()
-    schema
+    print(schema)
 except Exception as e:
-    f"ERROR: {str(e)}"
+    print(f"ERROR: {str(e)}")
       `;
       
       const schema = await pyodide.runPythonAsync(pythonScript);
       
+      alert(schema);
+
       if (schema.startsWith("ERROR:")) {
         throw new Error(schema);
       }
